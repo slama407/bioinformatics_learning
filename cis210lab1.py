b4936c9b15de3576b8bb7569e46cccdda765a07e
Sonam Lama <97857712+slama407@users.noreply.github.com>
2025-10-16 12:48:42 -0400

    Add files via upload

---
+++ cis210lab1.py
@@ -0,0 +1,34 @@
+"""
+CIS 210 Lab 1 – Lab 1 Exercises
+Author: [your name here]
+Credits: [acknowledgements - lab group, perhaps others]
+Lab exercises demonstrating how IDLE Editor and Shell interact.
+"""
+
+welcome = 'hello, Python'
+
+def is_even(n):
+
+    '''
+
+    (int) -> Boolean
+
+    Returns True if n
+
+    is an even number
+
+    >>> is_even(100)
+
+    True
+
+    >>> is_even(101)
+
+    False
+
+    >>> is_even(0)
+
+    True
+
+    '''
+
+    return (n % 2) == 0
